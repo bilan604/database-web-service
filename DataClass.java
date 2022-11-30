a283b7ef1f52183de15b59d54622d833ccf07837
Bill <77251582+bilan604@users.noreply.github.com>
2022-11-29 20:26:48 -0800

    Create DataClass.java

---
+++ DataClass.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class DataClass {
+  
+  private ArrayList<String> strings;
+
+  public void setStrings(ArrayList<String> strings) {
+    this.strings = strings;
+  }
+
+  public ArrayList<String> getStrings() {
+    return this.strings;
+  }
+  
+}
