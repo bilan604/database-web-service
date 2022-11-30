6ec215c9dcdf6d94b1aaba01a4c4f545c22130ec
Bill <77251582+bilan604@users.noreply.github.com>
2022-11-29 20:27:04 -0800

    Create QueryMatcher.java

---
+++ QueryMatcher.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+class QueryMatcher extends WebService {
+
+  @Override
+  void provideService(String searchWord, Database db, int dataClassNumber) {
+
+    ArrayList<String> products = db.dataClasses.get(dataClassNumber).getStrings();
+    
+    Collections.sort(products);
+    List<List<String>> ans = new ArrayList<>();
+    
+    for (int i = 1; i <= searchWord.length(); i++) {
+      String query = searchWord.substring(0, i);
+      List<String> query_matches = new ArrayList<>();
+      
+      for (String product : products) {
+        if (query.length() <= product.length()) {
+          if (query.equals(product.substring(0, i))) {
+            query_matches.add(product);
+            if (query_matches.size() == 3) {
+              break;
+            }
+          }
+        }
+      }
+      
+      ans.add(query_matches);
+    }
+    
+    // add to total usage cost
+    db.usageCost += 8.0f;
+    System.out.println(ans);
+    
+  }
+  
+}
